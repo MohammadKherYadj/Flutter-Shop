dfeeef2ce0c8bae302ecbcbc2d3beed6a43cff65
Waleed-Tabaa <hamzagareb55@gmail.com>
2023-08-28 10:48:58 +0300

    waleed update

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -10,7 +11,10 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(appBar: AppBar(title: Text("HomePage"),)),
+      child: Scaffold(
+          appBar: AppBar(
+        title: const Text("HomePage"),
+      )),
     );
   }
 }
