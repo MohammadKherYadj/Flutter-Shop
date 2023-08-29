466592469775487c34eb84dacff231dff5b7d390
MohammadKherYadj <mohammadkher509@gmail.com>
2023-08-29 11:41:49 -0900

    mohammad update

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const SignUp(),
+      home: const Login(),
       routes: {
         "home": (context) => const HomePage(),
         "login": (context) => const Login(),
